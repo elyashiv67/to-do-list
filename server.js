92fb9460a42cf9bc8511405068f961b9fd6d943c
elyashiv67 <elishivs5@gmail.com>
2025-08-22 10:25:32 +0300

    added a tasks array and a path to watch it

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 4365 ;
 
+
+const tasks = [{id:1,name:"task 1"}];
+
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/arr",(req,res)=>{
+res.json(tasks);
 });
 
 
